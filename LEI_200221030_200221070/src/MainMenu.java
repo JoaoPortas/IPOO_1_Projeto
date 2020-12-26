39d76b5f1679fd368b08b95f377ba73aabc33035
2Noob1 <joaogouveia143@outlook.pt>
2020-12-26 22:43:40 +0000

    fixed some shit

--- LEI_200221030_200221070/src/MainMenu.java
+++ LEI_200221030_200221070/src/MainMenu.java
@@ -32,13 +32,15 @@ public class MainMenu {
      * @param title Titlo a exibir
      * @param options Opções a exibir
      */
+    
+    
     private void printMenu(String title,String[] options){
         System.out.print('+');
         for (int a = 0; a < title.length() + 10;a++){
             System.out.print('-');
         }
         System.out.print("+\n");
-        System.out.println("     " + title + "              ");
+        System.out.println("     " + title);
         System.out.println("                                          ");
         for(int a = 0; a < options.length;a++){
             System.out.println("  " + (a + 1) + " - " + options[a]);
