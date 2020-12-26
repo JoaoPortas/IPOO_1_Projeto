f20d51b7eaf52acae35e21ff7fd4e5ef667f748a
2Noob1 <joaogouveia143@outlook.pt>
2020-12-26 13:59:43 +0000

    Adicionada um teste para um sistema de menu

--- LEI_200221030_200221070/src/MainMenu.java
+++ LEI_200221030_200221070/src/MainMenu.java
@@ -1,33 +1,83 @@
+
+
 public class MainMenu {
     
-    private InputReader reader;
-
-    public MainMenu(){
-        this.reader = new InputReader();
+    InputReader inputReader;
+    boolean isActive = false;
+    LEI_200221030_200221070 mainActivity;
+    int currentlyAvailableOptions;
+    public MainMenu(LEI_200221030_200221070 mainActivity){
+        if (this.isActive == false){
+            if (this.mainActivity == null){
+                this.mainActivity = mainActivity;
+                this.inputReader = new InputReader();
+            }
+        }
     }
 
-    /*
-    *   Mostra o Menu Principal
-    *   Esta função nao retorna nenhum valor
-    */
-    public void drawMenu(){
-        System.out.println("*************************************************");
-        System.out.println("*                                               *");
-        System.out.println("*       Bem-vindo á aplicação de restreio       *");
-        System.out.println("*                 de sala de aula               *");
-        System.out.println("*                                               *");
-        System.out.println("*                                               *");
-        System.out.println("*  1 - Registo de Presenças                     *");
-        System.out.println("*  2 - Área de Utilizador                       *");
-        System.out.println("*  3 - Área de Autoridade de Saúde              *");
-        System.out.println("*  4 - Área de Administrador                    *");
-        System.out.println("*  5 - Sair                                     *");
-        System.out.println("*                                               *");
-        System.out.println("*************************************************");
+    public void enableMenu(){
+        this.isActive = true;
+        this.menuHandler();
     }
 
-    public int getResponse(){
-        int returnValue =  this.reader.getIntegerNumber("Opção");
-        return returnValue;
+
+    public boolean isActive(){
+        return this.isActive;
     }
+
+    private void printMenu(String title,String[] options){
+        System.out.println("+-----------------------------------------+");
+        System.out.println("     " + title + "              ");
+        System.out.println("                                          ");
+        for(int a = 0; a < options.length;a++){
+            System.out.println("  " + (a + 1) + " - " + options[a]);
+        }
+        System.out.println("                                          ");
+        System.out.println("+-----------------------------------------+");
+    }
+
+    private void menuHandler(){
+        String[] options = {
+            "Registo de presenças",
+            "Área de utilizador",
+            "Área de Autoridade de Saúde",
+            "Área de Administrador",
+            "Sair"
+        };
+        printMenu("Bem Vindo ao Sistema de rastreio",options);
+        int response = getResponse();
+        switch(response){
+            case 1:
+                this.mainActivity.changeMenu(1);
+                break;
+            case 2:
+                this.mainActivity.changeMenu(1);
+                break;
+            case 3:
+                this.mainActivity.changeMenu(1);
+                break;
+            case 4:
+                this.mainActivity.changeMenu(1);
+                break;
+            case 5:
+                disableMenu();
+                break;
+            default:
+                System.out.println("Opção Inválida, tente novamente (prima 'enter' para continuar)");
+                this.inputReader.nextLine();
+                menuHandler();
+                break;
+        }
+    }
+
+    private void disableMenu(){
+        this.isActive = false;
+        this.mainActivity.closeApplication();
+    }
+
+
+    private int getResponse(){
+        return this.inputReader.getIntegerNumber("Opção");
+    }
+
 }
