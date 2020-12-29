4195578126734ba0399a572290b1d74809199a2c
Joao Portas <portas.joao.developer@gmail.com>
2020-12-29 17:15:35 +0000

    nova classe

---
+++ LEI_200221030_200221070/src/HealthOrganization.java
@@ -0,0 +1,13 @@
+/**
+ * Class da organização de saúde
+ * @author João Gouveia e João Portas
+ */
+public class HealthOrganization {
+
+    private User[] listOfInfecteds;
+    private Database baseDeDados;
+
+    public HealthOrganization() {
+
+    }
+}
